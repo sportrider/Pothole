b087809a0f9f687686fac69a2a811dfdf290bb2a
sportrider <garyjh99@gmail.com>
2020-12-03 14:14:23 -0500

    Initial Commit

---
+++ Pothole/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pothole
+//
+//  Created by Gary Harris on 12/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
